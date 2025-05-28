dda5c4017cb971b815247665ce0dfde1dca91c2f
Sharif185 <sharifssentaayi1@gmail.com>
2025-05-27 22:55:33 -0700

    Added University assignment

---
+++ first.py
@@ -0,0 +1,20 @@
+#print("Hello Python, I am now here for you")
+
+age  = 18
+if age >= 18:
+    print("You are an adult")
+elif age >=13:
+    print("You are a teenager")    
+else:
+    print("You are a child")
+    
+#Grade
+grade = 69
+if grade >=80:
+    print("A")
+elif grade >=70 & grade <=79:
+    print("B")     
+elif grade >= 60 & grade <=69:
+    print("c")  
+else:
+    print("D")         
